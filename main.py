2114da02d987e0eb3a3af29ef9c6fa1b11563b14
user <getup9@gmail.com>
2022-02-25 16:41:07 +0900

    네이버 로직 변경? 공감 아이디 가져올때 모바일 url 로 접속하도록 변경

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
-from bs4 import BeautifulSoup
 import urllib.request as req
+
+from bs4 import BeautifulSoup
+
 from PyQt5.QtWidgets import *
 from lib.BasicLayout import Ui_MainWindow
+
 
 class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -15,6 +18,7 @@ class Main(QMainWindow, Ui_MainWindow):
     def initUi(self):
         self.pb_sel.setEnabled(False)
         self.pte_idlist.clear()
+
 
     def initSignal(self):
         self.le_blogurl.textChanged.connect(self.chkUrlInput)
@@ -29,17 +33,30 @@ class Main(QMainWindow, Ui_MainWindow):
         blogurl = self.le_blogurl.text()
         naverid = blogurl.split('/')[3]
         blognumber = blogurl.split('/')[4]
-        url = "https://blog.naver.com/SympathyHistoryList.naver?blogId=" + naverid + "&logNo=" + blognumber + "&layoutWidthClassName=contw-966"
+        url = "https://m.blog.naver.com/SympathyHistoryList.naver?blogId=" + naverid + "&logNo=" + blognumber + "&layoutWidthClassName=contw-966"
 
         res = req.urlopen(url).read()
-        soup = BeautifulSoup(res, "html.parser")
-        list_li = soup.select("div#post-area > ul.list_sympathy > li.item > div > div > span > a.link.pcol2")
 
-        for li in list_li:
-            title = li.attrs['title']
-            href = li.attrs['href']
+        soup = BeautifulSoup(res, "html.parser")
+
+        # print(list_li)
+
+        try:
+            for href in soup.find_all("li"):
+                linkinfo = href.find("a")["href"]
+                print(linkinfo)
+                self.pte_idlist.appendPlainText(linkinfo.split('/')[3])
+        except:
+            print('예외가 발생했습니다.')
+
+
+
+        # for li in list_li:
+        #     title = li.attrs['title']
+        #     href = li.attrs['href']
+        #     print(href)
             # print(href.split('/')[3])
-            self.pte_idlist.appendPlainText(href.split('/')[3])
+        #     self.pte_idlist.appendPlainText(href.split('/')[3])
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
